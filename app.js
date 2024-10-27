ee69f1ad35bd458e05dc14aa24dd3888793df215
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-27 12:01:21 +0500

    class work
    class work

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// while loop
+// var i = 3; 
+// while (i <= 9){ 
+// console.log(i);
+// i++;
+// }
+
+
+// Do while loop
+// var i = 10;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 20)
+
+// do while loop
+// var i = 5;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= -10);
+
+
+
+// JavaScript Comment
+// var num = 2;
+// num++;
+// console.log(num);
+
+
+// JavaScript Multiple line Comment
+/* When a comment requires more than one line,
+ a block comment like this, with its opening
+ and closing tags, is the way to go. */
+
+
+//  function num(a,b){
+//     console.log(a+b);
+//  }
+
